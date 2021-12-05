ebac12b66055d7f0771be2ad476af6acc4ad64d9
faaiz <faaizasm@unist.ac.kr>
2021-12-05 20:54:45 +0900

    a

--- models/resnet_binary.py
+++ models/resnet_binary.py
@@ -6,8 +6,8 @@ __all__ = ['resnet']
 
 def conv3x3(in_planes, out_planes, stride=1):
     "3x3 convolution with padding"
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=1, bias=False)
+    return BinarizeConv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                          padding=1, bias=False)
 
 
 def init_model(model):
